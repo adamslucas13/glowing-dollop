6988e43c2d4aadf287cfee9ac4f0fb2daee47d22
Lucas Adams <37592304+adamslucas13@users.noreply.github.com>
2022-09-13 15:19:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//if else
+
+if (total > 100){
+    console.log("total: $" + total + "qualifies for a discount.");
+}else {
+    console.log(total);
+}
+
+//if else as ternary
+
+console.log(total > 100 ? 'qualifies for a discount':"does not qualifiy"); 
